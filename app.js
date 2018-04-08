3209dcb3a52f61ccf97683303e32c1d8147443e8
Hristo Spasov <hristo_sp99@abv.bg>
2018-04-08 18:22:14 +0300

    Connect app to database

--- app.js
+++ app.js
@@ -2,15 +2,26 @@ const Koa = require('koa');
 const logger = require('koa-logger');
 const bodyParser = require('koa-body');
 const serve = require('koa-static');
+const { Pool } = require('pg');
 const path = require('path');
 const routes = require('./routes');
 
+// Create app
 const app = new Koa();
+
+// Create a database connection pool
+const pool = new Pool({
+  user: process.env.PGUSER,
+  database: process.env.PGDATABASE,
+  password: process.env.PGPASSWORD,
+  port: process.env.PGPORT
+});
 
 app.use(logger());
 app.use(bodyParser());
 app.use(serve(path.join(__dirname, 'public')));
 
+app.context.db = pool;
 app.use(routes);
 
 app.listen(3000);
